33c1d82d9686c598ce1de253e14528430f9064b5
bek2518 <bereketb20@hotmail.com>
2023-04-27 10:53:59 +0300

    Define and annotate variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Defines and annotated variables with a specified values:
+a, an integer with a value of 1
+pi, a float with a value of 3.14
+i_understand_annotations, a boolean with a value of True
+school, a string with a value of “Holberton”
+'''
+from typing import NewType
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
