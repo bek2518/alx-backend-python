068d1ea3e1f5b61b2f523141be0383910e414ca1
bek2518 <bereketb20@hotmail.com>
2023-04-27 12:07:09 +0300

    Augment a given

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Augment the given code with correct duck-typed annotations
+'''
+from typing import Union, Any, Sequence, Type
+
+
+# The types of the elements of the input are not know
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, Type[None]]:
+    '''
+    Function to be augmented with lst argument which is a sequence of any type
+    and returns either an any type or nonetype
+    '''
+    if lst:
+        return lst[0]
+    else:
+        return None
