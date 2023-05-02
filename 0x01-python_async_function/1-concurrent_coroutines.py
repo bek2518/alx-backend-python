a7f466858a7c85b7b70c98ea5750222cf10f451d
bek2518 <bereketb20@hotmail.com>
2023-05-02 13:53:54 +0300

    Returns list of delays

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''
+
+'''
+import random
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    '''
+    Async routine takes int arguments n and max_delay, spawns wait_random
+    n times with specified max_delay and returns list of the delays in
+    ascending order without using sort()
+    '''
+    listOfDelay: list = []
+    sortedList: list = []
+    i: int = 0
+    while (i < n):
+        delay = await wait_random(max_delay)
+        listOfDelay.append(delay)
+        i += 1
+
+    def recurseSort(listOfDelay, sortedList):
+        '''
+        Function to sort the list in ascending order
+        '''
+        if (len(listOfDelay) != 0):
+            minimum = min(listOfDelay)
+            sortedList.append(minimum)
+            listOfDelay.remove(minimum)
+            return recurseSort(listOfDelay, sortedList)
+        else:
+            return sortedList
+
+    recurseSort(listOfDelay, sortedList)
+
+    return sortedList
