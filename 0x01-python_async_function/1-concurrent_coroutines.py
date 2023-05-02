6d4271a6c96204db03043081fc2f471e6e89452c
bek2518 <bereketb20@hotmail.com>
2023-05-02 14:26:01 +0300

    Changed to list of floats

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,17 +3,18 @@
 
 '''
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
     Async routine takes int arguments n and max_delay, spawns wait_random
     n times with specified max_delay and returns list of the delays in
     ascending order without using sort()
     '''
-    listOfDelay: list = []
-    sortedList: list = []
+    listOfDelay: List[float] = []
+    sortedList: List[float] = []
     i: int = 0
 
     listOfDelay = [wait_random(max_delay) for i in range(0, n)]
