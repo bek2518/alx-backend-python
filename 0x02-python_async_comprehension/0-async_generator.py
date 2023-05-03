5bb6f8b5dffe280da393513884481224b42526db
bek2518 <bereketb20@hotmail.com>
2023-05-03 09:54:58 +0300

    Yield async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Coroutine that loops 10 times and return random number
+'''
+import random
+import asyncio
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    '''
+    Coroutine that takes no arguments, which loops 10 times asynchronously
+    waits 1 second then yield a random number between 0 and 10
+    '''
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,17 @@
+# 0x02-python_async_comprehension
+
+## Description
+
+Introduces to asynchronous comprehension
+
+## Learning Objective
+
+* How to write an asynchronous generator
+* How to use async comprehensions
+* How to type-annotate generators
+
+### Files
+
+---
+No | File | Description
+---|---|---
