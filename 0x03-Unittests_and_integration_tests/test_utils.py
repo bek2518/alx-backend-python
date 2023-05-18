914ad1e906c7288bcf071a54bdbe9b06cfc57572
bek2518 <bereketb20@hotmail.com>
2023-05-18 15:18:02 +0300

    Tests if the method raises exceptoion

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,3 +20,18 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         '''Method that tests the provided method (access_nested_map)'''
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        '''
+        Tests if the method access_nested_map raises KeyError
+        '''
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
+
+    if __name__ == '__main__':
+        unittest.main()
