fc317d987c01c0b9a8a4501d67c813c3fe230333
bek2518 <bereketb20@hotmail.com>
2023-05-22 15:31:55 +0300

    Test for public_repo_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ Tests the GithubOrgClient class
 '''
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 
 
@@ -30,6 +30,17 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(github_org_client.org, test_payload)
             mock_thing.assert_called_once_with(GithubOrgClient.ORG_URL.
                                                format(org=org_examples))
+
+    def test_public_repos_url(self):
+        '''
+        Method that tests _public_repos_url of the GithubOrgClient
+        '''
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) \
+                as mock_thing:
+            repo_url = 'https://api.github.com/orgs/google/repos'
+            mock_thing.return_value = {"repos_url": repo_url}
+            result = GithubOrgClient('google')._public_repos_url
+            self.assertEqual(result, repo_url)
 
     if __name__ == '__main__':
         unittest.main()
